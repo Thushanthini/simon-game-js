f02f6dfdba29215daf78a7dba8f92784290d73cd
Thush <uthushanthini@gmail.com>
2024-11-09 14:42:50 +0530

    Add game over functionality

--- game.js
+++ game.js
@@ -32,7 +32,12 @@ function checkAnswer(currentLevel) {
       }, 1000);
     }
   } else {
-    console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("#level-title").text("Game Over, Press Any Key to Restart");
   }
 }
 
